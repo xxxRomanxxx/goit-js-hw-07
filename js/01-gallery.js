b276ea84dc959888a1c32e354671b81e09b30b8b
Roman Pediash <peaktib@gmail.com>
2023-02-12 15:09:36 +0200

    starting

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("afterbegin", galleryMarkup);
+galleryContainer.addEventListener('click', onGalleryCardClick)
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+        <div class = "gallery__item">
+        <a class = "gallery__link" href = "${original}"> 
+        <img class = "gallery__image"
+        src = "${preview}"
+        data-sourse = "${original}"
+        alt = "${description}"
+        />
+        </a>
+        </div>
+        `
+        }).join("");
+}
+
+function onGalleryCardClick(evt) {
+    evt.preventDefault();
+    const isGaleryImage = evt.target.classList.contains(".gallery__image");
+    if (!isGaleryImage) {
+        return;
+    }
+    console.log(evt.target)
+}
 console.log(galleryItems);
+
+
+
